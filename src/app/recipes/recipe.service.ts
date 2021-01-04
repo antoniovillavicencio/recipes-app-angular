1e41c19ac609d2733780aa77a5931a9b8fc6977d
Antonio Villavicencio <tonyvcj@gmail.com>
2021-01-04 17:10:18 -0600

    Implement ShoppingListService Add ingredients from a recipe to a shopping list

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,19 +1,37 @@
-import {EventEmitter} from '@angular/core';
+import {EventEmitter, Injectable} from '@angular/core';
 import {Recipe} from './recipe.model';
+import {Ingredient} from '../shared/ingredient.model';
+import {ShoppingListService} from '../shopping-list/shopping-list.service';
 
+@Injectable()
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
 
   recipes: Recipe[] = [
-    new Recipe('Test Recipe',
-      'Description of Test Recipe',
-      'https://cdn.pixabay.com/photo/2015/09/16/20/10/dough-943245_960_720.jpg'),
-    new Recipe('Another Recipe',
-      'Description of another Test Recipe',
-      'https://cdn.pixabay.com/photo/2015/09/16/20/10/dough-943245_960_720.jpg')
+    new Recipe('Chilaquiles',
+      'Description of Tasty Chilaquiles',
+      'https://cdn.pixabay.com/photo/2015/09/16/20/10/dough-943245_960_720.jpg',
+    [
+      new Ingredient('Tortillas', 300),
+      new Ingredient('Salsa verde', 250)
+    ]),
+    new Recipe('Fat Joe Burger',
+      'A Tasty Burger with lots of fat',
+      'https://cdn.pixabay.com/photo/2015/09/16/20/10/dough-943245_960_720.jpg',
+      [
+        new Ingredient('Angus Meat', 3),
+        new Ingredient('Cheddar Cheese', 2),
+        new Ingredient('Bacon', 3),
+      ])
   ];
 
   getRecipes(): Recipe[] {
     return this.recipes.slice();
+  }
+
+  constructor(private slService: ShoppingListService) {}
+
+  addIngredientsToShoppingList(ingredients: Ingredient[]): void {
+    this.slService.addIngredients(ingredients);
   }
 }
--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,3 +1,25 @@
-export class ShoppingListService {
+import { EventEmitter } from '@angular/core';
+import {Ingredient} from '../shared/ingredient.model';
 
+export class ShoppingListService {
+  ingredientsChanged = new EventEmitter<Ingredient[]>();
+
+  ingredients: Ingredient[] = [
+    new Ingredient('Apple', 2),
+    new Ingredient('Bananas', 3),
+  ];
+
+  getIngredients(): Ingredient[] {
+    return this.ingredients.slice();
+  }
+
+  addIngredient(ingredient: Ingredient): void {
+    this.ingredients.push(ingredient);
+    this.ingredientsChanged.emit(this.ingredients.slice());
+  }
+
+  addIngredients(ingredients: Ingredient[]): void {
+    this.ingredients.push(...ingredients);
+    this.ingredientsChanged.emit(this.ingredients.slice());
+  }
 }
