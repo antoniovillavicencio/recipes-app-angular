e36d46e06329beae0496135c4ee8bcf6eee55320
Antonio Villavicencio <tonyvcj@gmail.com>
2021-01-03 19:06:09 -0600

    Add directive to enable dropdowns

---
+++ src/app/shared/dropdown.directive.ts
@@ -0,0 +1,20 @@
+import {Directive, ElementRef, HostBinding, HostListener, Renderer2} from '@angular/core';
+
+@Directive({
+  selector: '[appDropdown]'
+})
+export class DropdownDirective {
+  @HostBinding('class.show') isOpen = false;
+
+  constructor(private el: ElementRef, private renderer: Renderer2) {}
+
+  @HostListener('click') toggleOpen(): void {
+    this.isOpen = !this.isOpen;
+    const part = this.el.nativeElement.querySelector('.dropdown-menu');
+    if(this.isOpen) {
+      this.renderer.addClass(part, 'show');
+    } else {
+      this.renderer.removeClass(part, 'show');
+    }
+  }
+}
